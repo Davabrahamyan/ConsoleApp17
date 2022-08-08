a5fe91f43c2c96a90d4c053f2e35c0797b82c14f
vahra <vahra@DESKTOP-SO0L1VA>
2022-08-08 18:33:25 +0300

    Add project files.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 8;
+try
+{
+
+    Console.WriteLine( a / 0 ); 
+    
+}
+catch (Exception n)
+{
+    Console.WriteLine($"exception-{n}");
+}
